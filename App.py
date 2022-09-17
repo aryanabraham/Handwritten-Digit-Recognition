2c8e6163f48ed3ac94cd888f5fe6b94ca6b6136d
Aryan Abraham <abraham.aryan01@gmail.com>
2022-09-18 04:55:50 +0530

    Defined sytem loop

--- App.py
+++ App.py
@@ -7,10 +7,28 @@ import cv2
 WINDOWSIZEX = 640
 WINDOWSIZEY = 480
 
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+
+IMAGESAVE = False
+
+MODEL = load_model("Digit Recognition//bestmodel.h5")
+
+LABELS = {0: "Zero", 1: "One",
+          2: "Two", 3: "Three",
+          4: "Four", 5: "Five",
+          6: "Six", 7: "Seven",
+          8: "Eight", 9: "Nine"}
+
 # Initialize pygame
 pygame.init()
 
+#FONT = pygame.font.Font("freesansbold.tff", 18)
 pygame.display.set_mode((WINDOWSIZEX, WINDOWSIZEY))
+pygame.display.set_caption("Digit Board")
+
+iswriting = False
 
 while True: 
 
@@ -18,3 +36,12 @@ while True:
         if event.type == QUIT:
               pygame.quit()
               sys.exit()
+
+        if event.type = MOUSEMOTION and iswriting:
+            xcord, ycor = event.pos
+            pygame.draw.circle(DISPLAYSURF, WHITE, (xcord, ycord), 4, 0)
+            
+        
+        if event.type == MOUSEBUTTONDOWN:
+            iswriting = True
+            
